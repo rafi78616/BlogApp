a8a5d97af5c76d94934c8757617ccadcc30cdce3
rafi78616 <rafiraj78616@gmail.com>
2024-01-14 15:30:48 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./routes/user-routes";
+import blogRouter from "./routes/blog-routes";
+
 
 const app = express();
+app.use(express.json());
+app.use("/api/user",router);
+app.use("/api/blog",blogRouter);
 
 mongoose.connect('mongodb+srv://admin:laOokPGPNxlnE0lZ@cluster0.khdhf5l.mongodb.net/Blog?retryWrites=true&w=majority'
 ).then(() => app.listen(5000))
