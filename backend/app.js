e486e3ee39a119dc8cd375078673d6f21102a1a1
rafi78616 <rafiraj78616@gmail.com>
2024-01-17 13:14:26 +0530

    frontend

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes";
 import blogRouter from "./routes/blog-routes";
-
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/user",router);
 app.use("/api/blog",blogRouter);
@@ -12,4 +13,4 @@ app.use("/api/blog",blogRouter);
 mongoose.connect('mongodb+srv://admin:laOokPGPNxlnE0lZ@cluster0.khdhf5l.mongodb.net/Blog?retryWrites=true&w=majority'
 ).then(() => app.listen(5000))
  .then(() => console.log("Connected to mongodb and listening to server")
-).catch((err) => console.log(err));
+).catch((err) => console.log(err)); 
--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const getAllBlogs = async(req,res,next) =>{
     let blogs;
     try{
-        blogs = await Blog.find();
+        blogs = await Blog.find().populate("user");
     }catch(err){
         return console.log(err);
     }
@@ -109,5 +109,5 @@ export const getByUserId = async(req,res,next) =>{
     if(!userBlogs){
         return res.status(404).json({message : "No Blogs Found"})
     }
-    return res.status(200).json({blogs : userBlogs})
+    return res.status(200).json({user : userBlogs})
 }
